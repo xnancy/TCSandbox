84a7840425f5a45cf34af4a4c0045bab3a4b75b1
Savannah McCoy <savannahm@fb.com>
2016-07-08 14:40:42 -0700

    collection view changes

---
+++ TCSandbox/Gifs2CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  Gifs2CollectionViewCell.swift
+//  TCSandbox
+//
+//  Created by Savannah McCoy on 7/8/16.
+//  Copyright © 2016 Nancy Xu. All rights reserved.
+//
+
+import UIKit
+
+class Gifs2CollectionViewCell: UICollectionViewCell {
+    
+}
+
+
+
+
