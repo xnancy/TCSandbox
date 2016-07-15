dcb46bf6611fb8a2c2a889ad3ffc66201000bb1b
Savannah McCoy <savannahm@fb.com>
2016-07-14 18:16:40 -0700

    No major Changes. Just pulling & pushing before I leave

--- TCSandbox/WorkoutEditorViewController.swift
+++ TCSandbox/WorkoutEditorViewController.swift
@@ -38,11 +38,12 @@ class WorkoutEditorViewController: UIViewController {
     var gifmanager = SwiftyGifManager(memoryLimit: 20)
     //var gifsDictionary: Dictionary()
     //var gifsToShow: [String] = []
-    var gifsToShow: [String?] = []
+    var gifsToShow: [String] = []
     var tagsToShow: [String] = []
     var movesCount: Int!
     var workoutCount: Int!
     var tagsCount: Int!
+    var timeLimit: Int
     //var gifDescriptionsToShow:[String] = []
     // var gifDescriptionsToShow: [String] = []
     // var index = Int()
@@ -68,31 +69,31 @@ class WorkoutEditorViewController: UIViewController {
         //print(tagsToShow)
         
         
-        selectedMoveLabel1.text = Gifs.gifDictionary[(gifsToShow[0])!]
-        gifSelectedImageView1.image = UIImage(named: gifsToShow[0]!)
+        selectedMoveLabel1.text = Gifs.gifDictionary[(gifsToShow[0])]
+        gifSelectedImageView1.image = UIImage(named: gifsToShow[0])
         
         
         if workoutCount > 1 {
         
         if workoutCount == 2 {
-        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])!]
-        gifSelectedImageView2.image = UIImage(named: gifsToShow[1]!)
+        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])]
+        gifSelectedImageView2.image = UIImage(named: gifsToShow[1])
         }
         
         else if workoutCount == 3 {
-        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])!]
-        gifSelectedImageView2.image = UIImage(named: gifsToShow[1]!)
-        selectedMoveLabel3.text = Gifs.gifDictionary[(gifsToShow[2])!]
-        gifSelectedImageView4.image = UIImage(named: gifsToShow[2]!)
+        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])]
+        gifSelectedImageView2.image = UIImage(named: gifsToShow[1])
+        selectedMoveLabel3.text = Gifs.gifDictionary[(gifsToShow[2])]
+        gifSelectedImageView4.image = UIImage(named: gifsToShow[2])
         }
         
         else if workoutCount == 4 {
-        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])!]
-        gifSelectedImageView2.image = UIImage(named: gifsToShow[1]!)
-        selectedMoveLabel3.text = Gifs.gifDictionary[(gifsToShow[2])!]
-        gifSelectedImageView4.image = UIImage(named: gifsToShow[2]!)
-        selectedMoveLabel4.text = Gifs.gifDictionary[(gifsToShow[3])!]
-        gifSelectedImageView3.image = UIImage(named: gifsToShow[3]!)
+        selectedMoveLabel2.text = Gifs.gifDictionary[(gifsToShow[1])]
+        gifSelectedImageView2.image = UIImage(named: gifsToShow[1])
+        selectedMoveLabel3.text = Gifs.gifDictionary[(gifsToShow[2])]
+        gifSelectedImageView4.image = UIImage(named: gifsToShow[2])
+        selectedMoveLabel4.text = Gifs.gifDictionary[(gifsToShow[3])]
+        gifSelectedImageView3.image = UIImage(named: gifsToShow[3])
         }
             
             
@@ -103,7 +104,6 @@ class WorkoutEditorViewController: UIViewController {
         if tagsCount > 0 {
         
         if workoutCount == 1 {
-        
             selectedMoveLabel2.text = Tags.tagDictionary[tagsToShow[0]]
             gifSelectedImageView2.image = UIImage(named: tagsToShow[0])
             
@@ -111,6 +111,7 @@ class WorkoutEditorViewController: UIViewController {
                 selectedMoveLabel3.text = Tags.tagDictionary[tagsToShow[1]]
                 gifSelectedImageView4.image = UIImage(named: tagsToShow[1])
             }
+                
             else if tagsCount == 3{
                 selectedMoveLabel3.text = Tags.tagDictionary[tagsToShow[1]]
                 gifSelectedImageView4.image = UIImage(named: tagsToShow[1])
@@ -121,42 +122,31 @@ class WorkoutEditorViewController: UIViewController {
             
         }
         else if workoutCount == 2 {
-            
             selectedMoveLabel3.text = Tags.tagDictionary[tagsToShow[0]]
             gifSelectedImageView4.image = UIImage(named: tagsToShow[0])
             
             if tagsCount == 2{
                 selectedMoveLabel4.text = Tags.tagDictionary[tagsToShow[1]]
                 gifSelectedImageView3.image = UIImage(named: tagsToShow[1])
-//                selectedMoveLabel4.text = Tags.tagDictionary[tagsToShow[2]]
-//                gifSelectedImageView3.image = UIImage(named: tagsToShow[2])
-
             }
     
         }
         else if workoutCount == 3 {
             selectedMoveLabel4.text = Tags.tagDictionary[tagsToShow[0]]
             gifSelectedImageView3.image = UIImage(named: tagsToShow[0])
-            
         }
         
         }
-       
-        
-       
-        
-        //        */
-        //print(index)
-        //print(gifsToShow)
-        ///print(gifDescriptionsToShow)
         
         
-        //challenge = Challenge()
         
-        //print(challenge?.challengeName)
-        // selectedMoveLabel1.text = gifsToShow[0]
+ //========================================== CHALLENGE OBJECT INIT ============================================
+//        challenge = Challenge(timeLimit: timeLimit, gifNames: gifsToShow, tagNames: tagsToShow, deadline: NSDate, participants: [String])
+        
+        
     }
-    
+//==============================================================================================================
+   
     @IBAction func backButton(sender: AnyObject) {
         
         dismissViewControllerAnimated(true, completion: nil)
