52fa920eff6d561e9112819f5f64c287654b542d
Savannah McCoy <savannahm@fb.com>
2016-07-18 13:08:50 -0700

    Began working on DetailVC for user challenges

---
+++ TCSandbox/ChallengeDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ChallengeDetailViewController.swift
+//  TCSandbox
+//
+//  Created by Savannah McCoy on 7/18/16.
+//  Copyright © 2016 Nancy Xu. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.tabBar.hidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
