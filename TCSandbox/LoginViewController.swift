78088d397fe55cf9492d7840ed046be6f4cf0adf
Savannah McCoy <savannahm@fb.com>
2016-07-29 17:02:25 -0700

    Working on Onboarding pop up

--- TCSandbox/LoginViewController.swift
+++ TCSandbox/LoginViewController.swift
@@ -57,10 +57,13 @@ class LoginViewController: UIViewController,
     var arrayOfTitle = ["CREATE AN ACCOUNT", "ADD YOUR FRIENDS", "CREATE A CHALLENGE", "RECORD AND SHARE"]
     var arrayOfDescription = [
         
-        "In your profile, you can view the statistics of its operations and the recommandations of friends",
-        "Purchase tickets on hot tours to your favorite planet and fly to the most comfortable intergalactic spaceships of best companies",
-        "In the process of flight you will be in cryogenic sleep and supply the body with all the necessary things for life",
-        "Lit"
+        "Sign Up for TapeRace with your Facebook account! If you dont have a Facebook account, you can make one by clicking the 'Register with Facebook' button on our login screen.",
+        
+        "Add your friends to see their challenges in your feed as well as to send them awesome challenges you create. Once they're your friends in the app, they can send you sweet challenges too!",
+        
+        "To create a challenge, click on the plus sign icon and choose from over 200 different moves and tags. Choose at least one move to base your challenge and add tags to spice it up!",
+        
+        "After creating your challenge, record a video of yourself completing the challenge then select friends to share it with. Then TAAAADAAA, You've created a challenge!!!"
     
     ]
   
